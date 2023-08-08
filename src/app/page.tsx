b3ddf353d36e209772f481e0ad556a78f0891f69
Jack Sibley <jacks@sibley.com.au>
2023-08-08 22:32:09 +1000

    fixed colours

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,15 +2,15 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <main className="bg-slate-900 text-slate-400 selection:bg-teal-300 selection:text-teal-900 leading-relaxed antialiased">
-      <div className="fon-sans mx-auto min-h-screen max-w-screen-xl px-6 py-12 md:px-12 md:py-20 lg:px-24 lg:py-0">
+    <body className="bg-slate-900 leading-relaxed text-slate-400 antialiased selection:bg-teal-300 selection:text-teal-900">
+      <div className="container mx-auto min-h-screen max-w-screen-xl px-6 py-12 font-sans md:px-12 md:py-20 lg:px-24 lg:py-0">
         <div className="lg:flex lg:justify-between lg:gap-4">
           <header className="lg:sticky lg:top-0 lg:flex lg:max-h-screen lg:w-1/2 lg:flex-col lg:justify-between lg:py-24">
             <div>
-              <h1 className="text-slate-200 text-4xl font-bold tracking-tight sm:text-5xl">
+              <h1 className="text-4xl font-bold tracking-tight text-slate-200 sm:text-5xl">
                 <a href="/"> Jack Sibley</a>
               </h1>
-              <h2 className="text-slate-200 mt-3 text-lg font-medium tracking-tight sm:text-xl">
+              <h2 className="fo mt-3 text-lg tracking-tight text-slate-200 sm:text-xl">
                 Graduate Software Engineer
               </h2>
               <p className="mt-4 max-w-xs leading-normal">
@@ -27,8 +27,8 @@ export default function Home() {
                       className="active group flex items-center py-3"
                       href="#about"
                     >
-                      <span className="nav-indicator bg-slate-600 group-hover:bg-slate-200 group-focus-visible:bg-slate-200 mr-4 h-px w-8 transition-all group-hover:w-16 group-focus-visible:w-16 motion-reduce:transition-none"></span>
-                      <span className="nav-text text-slate-500 group-hover:text-slate-200 group-focus-visible:text-slate-200 text-xs font-bold uppercase tracking-widest">
+                      <span className="nav-indicator mr-4 h-px w-8 bg-slate-600 transition-all group-hover:w-16 group-hover:bg-slate-200 group-focus-visible:w-16 group-focus-visible:bg-slate-200 motion-reduce:transition-none"></span>
+                      <span className="nav-text text-xs font-bold uppercase tracking-widest text-slate-500 group-hover:text-slate-200 group-focus-visible:text-slate-200">
                         About
                       </span>
                     </a>
@@ -38,8 +38,8 @@ export default function Home() {
                       className="group flex items-center py-3"
                       href="#experience"
                     >
-                      <span className="nav-indicator bg-slate-600 group-hover:bg-slate-200 group-focus-visible:bg-slate-200 mr-4 h-px w-8 transition-all group-hover:w-16 group-focus-visible:w-16 motion-reduce:transition-none"></span>
-                      <span className="nav-text text-slate-500 group-hover:text-slate-200 group-focus-visible:text-slate-200 text-xs font-bold uppercase tracking-widest">
+                      <span className="nav-indicator mr-4 h-px w-8 bg-slate-600 transition-all group-hover:w-16 group-hover:bg-slate-200 group-focus-visible:w-16 group-focus-visible:bg-slate-200 motion-reduce:transition-none"></span>
+                      <span className="nav-text text-xs font-bold uppercase tracking-widest text-slate-500 group-hover:text-slate-200 group-focus-visible:text-slate-200">
                         Experience
                       </span>
                     </a>
@@ -49,8 +49,8 @@ export default function Home() {
                       className="group flex items-center py-3"
                       href="#projects"
                     >
-                      <span className="nav-indicator bg-slate-600 group-hover:bg-slate-200 group-focus-visible:bg-slate-200 mr-4 h-px w-8 transition-all group-hover:w-16 group-focus-visible:w-16 motion-reduce:transition-none"></span>
-                      <span className="nav-text text-slate-500 group-hover:text-slate-200 group-focus-visible:text-slate-200 text-xs font-bold uppercase tracking-widest">
+                      <span className="nav-indicator mr-4 h-px w-8 bg-slate-600 transition-all group-hover:w-16 group-hover:bg-slate-200 group-focus-visible:w-16 group-focus-visible:bg-slate-200 motion-reduce:transition-none"></span>
+                      <span className="nav-text text-xs font-bold uppercase tracking-widest text-slate-500 group-hover:text-slate-200 group-focus-visible:text-slate-200">
                         Projects
                       </span>
                     </a>
@@ -61,6 +61,6 @@ export default function Home() {
           </header>
         </div>
       </div>
-    </main>
+    </body>
   );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,5 @@
 /** @type {import('tailwindcss').Config} */
+
 module.exports = {
   content: [
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
@@ -9,18 +10,6 @@ module.exports = {
     "./src/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
-    colors: {
-      transparent: "transparent",
-      current: "currentColor",
-      white: "#ffffff",
-      purple: "#3f3cbb",
-      midnight: "#121063",
-      metal: "#565584",
-      tahiti: "#3ab7bf",
-      silver: "#ecebff",
-      "bubble-gum": "#ff77e9",
-      bermuda: "#78dcca",
-    },
     extend: {},
   },
   plugins: [],

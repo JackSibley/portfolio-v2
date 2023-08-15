a2a50e480d0d589dcd089937b4b575eb745c8468
Jack Sibley <jacks@sibley.com.au>
2023-08-15 19:17:31 +1000

    updated metadata for SEO

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -12,6 +12,42 @@ export const metadata: Metadata = {
   title: "Jack Sibley",
   description:
     "Jack Sibley is Crafting digital dreams with code. From classroom to compiler, I turn concepts into elegant software solutions.",
+  keywords: [
+    "graduate software engineer",
+    "portfolio",
+    "projects",
+    "skills",
+    "software development",
+    "web development",
+    "coding",
+    "programming",
+    "full stack",
+  ],
+  authors: [{ name: "Jack Sibley" }],
+  creator: "Jack Sibley",
+  twitter: {
+    card: "summary_large_image",
+    title: "Jack Sibley",
+    description:
+      "Jack Sibley is Crafting digital dreams with code. From classroom to compiler, I turn concepts into elegant software solutions.",
+    site: "@jsibleyau",
+    siteId: "1478756861029478403",
+    creator: "@jsibleyau",
+    creatorId: "1478756861029478403",
+    images: [
+      "https://pbs.twimg.com/profile_images/1501356688427073536/eGwrAQ02_400x400.jpg",
+    ],
+  },
+  openGraph: {
+    title: "Jack Sibley",
+    description:
+      "Jack Sibley is Crafting digital dreams with code. From classroom to compiler, I turn concepts into elegant software solutions.",
+    images:
+      "https://pbs.twimg.com/profile_images/1501356688427073536/eGwrAQ02_400x400.jpg",
+    type: "website",
+    url: "https://www.jacksibley.dev/",
+  },
+  robots: "index, follow",
 };
 
 export default function RootLayout({

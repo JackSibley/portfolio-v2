c493b673a63a358b5f5b97464ce9077a963392e2
Jack Sibley <jacks@sibley.com.au>
2023-08-15 17:23:45 +1000

    added experience and project sections.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,7 @@
+"use client";
 import Image from "next/image";
 import { TextRevealTW } from "./../../components/TextReveal";
+import { Nav } from "./../../components/Nav";
 
 export default function Home() {
   return (
@@ -22,46 +24,7 @@ export default function Home() {
                 Crafting digital dreams with code. From classroom to compiler, I
                 turn concepts into elegant software solutions.{" "}
               </p>
-              <nav
-                className="nav hidden lg:block"
-                aria-label="In-page jump links"
-              >
-                <ul className="mt-16 w-max">
-                  <li>
-                    <a
-                      className="active group flex items-center py-3"
-                      href="#about"
-                    >
-                      <span className="nav-indicator mr-4 h-px w-8 bg-slate-600 transition-all group-hover:w-16 group-hover:bg-slate-200 group-focus-visible:w-16 group-focus-visible:bg-slate-200 motion-reduce:transition-none"></span>
-                      <span className="nav-text text-xs font-bold uppercase tracking-widest text-slate-500 group-hover:text-slate-200 group-focus-visible:text-slate-200">
-                        About
-                      </span>
-                    </a>
-                  </li>
-                  <li>
-                    <a
-                      className="group flex items-center py-3"
-                      href="#experience"
-                    >
-                      <span className="nav-indicator mr-4 h-px w-8 bg-slate-600 transition-all group-hover:w-16 group-hover:bg-slate-200 group-focus-visible:w-16 group-focus-visible:bg-slate-200 motion-reduce:transition-none"></span>
-                      <span className="nav-text text-xs font-bold uppercase tracking-widest text-slate-500 group-hover:text-slate-200 group-focus-visible:text-slate-200">
-                        Experience
-                      </span>
-                    </a>
-                  </li>
-                  <li>
-                    <a
-                      className="group flex items-center py-3"
-                      href="#projects"
-                    >
-                      <span className="nav-indicator mr-4 h-px w-8 bg-slate-600 transition-all group-hover:w-16 group-hover:bg-slate-200 group-focus-visible:w-16 group-focus-visible:bg-slate-200 motion-reduce:transition-none"></span>
-                      <span className="nav-text text-xs font-bold uppercase tracking-widest text-slate-500 group-hover:text-slate-200 group-focus-visible:text-slate-200">
-                        Projects
-                      </span>
-                    </a>
-                  </li>
-                </ul>
-              </nav>
+              {Nav()}
             </div>
             <ul
               className="ml-1 mt-8 flex items-center"
@@ -197,7 +160,203 @@ export default function Home() {
                 </h2>
               </div>
               <div>
-                <ol className="group/list"></ol>
+                <ol className="group/list">
+                  <li className="mb-12">
+                    <div className="group relative grid pb-1 transition-all sm:grid-cols-8 sm:gap-8 md:gap-4 lg:hover:!opacity-100 lg:group-hover/list:opacity-50">
+                      <div className="absolute -inset-x-4 -inset-y-4 z-0 hidden rounded-md transition motion-reduce:transition-none lg:-inset-x-6 lg:block lg:group-hover:bg-slate-800/50 lg:group-hover:shadow-[inset_0_1px_0_0_rgba(148,163,184,0.1)] lg:group-hover:drop-shadow-lg"></div>
+                      <header
+                        className="z-10 mb-2 mt-1 text-xs font-semibold uppercase tracking-wide text-slate-500 sm:col-span-2"
+                        aria-label="2018 to Present"
+                      >
+                        2018 — Present
+                      </header>
+                      <div className="z-10 sm:col-span-6">
+                        <h3 className="font-medium leading-snug text-slate-200">
+                          <div>
+                            <a
+                              className="group/link inline-flex items-baseline text-base font-medium leading-tight text-slate-200  hover:text-teal-300 focus-visible:text-teal-300"
+                              href="https://www.flndrs.com.au/"
+                              target="_blank"
+                              rel="noreferrer"
+                              aria-label="Event Officer at FLNDRS"
+                            >
+                              <span className="absolute -inset-x-4 -inset-y-2.5 hidden rounded md:-inset-x-6 md:-inset-y-4 lg:block"></span>
+                              <span>
+                                DJ ·{" "}
+                                <span className="inline-block">
+                                  FLNDRS Bar & Nightclub
+                                  <svg
+                                    xmlns="http://www.w3.org/2000/svg"
+                                    viewBox="0 0 20 20"
+                                    fill="currentColor"
+                                    className="ml-1 inline-block h-4 w-4 shrink-0 translate-y-px transition-transform group-hover/link:-translate-y-1 group-hover/link:translate-x-1 group-focus-visible/link:-translate-y-1 group-focus-visible/link:translate-x-1 motion-reduce:transition-none"
+                                    aria-hidden="true"
+                                  >
+                                    <path
+                                      fill-rule="evenodd"
+                                      d="M5.22 14.78a.75.75 0 001.06 0l7.22-7.22v5.69a.75.75 0 001.5 0v-7.5a.75.75 0 00-.75-.75h-7.5a.75.75 0 000 1.5h5.69l-7.22 7.22a.75.75 0 000 1.06z"
+                                      clip-rule="evenodd"
+                                    ></path>
+                                  </svg>
+                                </span>
+                              </span>
+                            </a>
+                          </div>
+                          <div>
+                            <div className="text-slate-500" aria-hidden="true">
+                              Event Officer
+                            </div>
+                          </div>
+                        </h3>
+                        <p className="mt-2 text-sm leading-normal">
+                          Proficient event planner responsible for end-to-end
+                          event execution, including equipment selection,
+                          audience interaction, client communication, equipment
+                          maintenance, schedule management, budget control, and
+                          networking.
+                        </p>
+                      </div>
+                    </div>
+                  </li>
+                  <li className="mb-12">
+                    <div className="group relative grid pb-1 transition-all sm:grid-cols-8 sm:gap-8 md:gap-4 lg:hover:!opacity-100 lg:group-hover/list:opacity-50">
+                      <div className="absolute -inset-x-4 -inset-y-4 z-0 hidden rounded-md transition motion-reduce:transition-none lg:-inset-x-6 lg:block lg:group-hover:bg-slate-800/50 lg:group-hover:shadow-[inset_0_1px_0_0_rgba(148,163,184,0.1)] lg:group-hover:drop-shadow-lg"></div>
+                      <header
+                        className="z-10 mb-2 mt-1 text-xs font-semibold uppercase tracking-wide text-slate-500 sm:col-span-2"
+                        aria-label="July to December 2017"
+                      >
+                        May 2016 — July 2017
+                      </header>
+                      <div className="z-10 sm:col-span-6">
+                        <h3 className="font-medium leading-snug text-slate-200">
+                          <div>
+                            <a
+                              className="group/link inline-flex items-baseline text-base font-medium leading-tight text-slate-200  hover:text-teal-300 focus-visible:text-teal-300"
+                              href="https://www.pumaenergy.com.au/"
+                              target="_blank"
+                              rel="noreferrer"
+                              aria-label="Console Operator at Puma"
+                            >
+                              <span className="absolute -inset-x-4 -inset-y-2.5 hidden rounded md:-inset-x-6 md:-inset-y-4 lg:block"></span>
+                              <span>
+                                Console Operator ·{" "}
+                                <span className="inline-block">
+                                  Puma Energy
+                                  <svg
+                                    xmlns="http://www.w3.org/2000/svg"
+                                    viewBox="0 0 20 20"
+                                    fill="currentColor"
+                                    className="ml-1 inline-block h-4 w-4 shrink-0 translate-y-px transition-transform group-hover/link:-translate-y-1 group-hover/link:translate-x-1 group-focus-visible/link:-translate-y-1 group-focus-visible/link:translate-x-1 motion-reduce:transition-none"
+                                    aria-hidden="true"
+                                  >
+                                    <path
+                                      fill-rule="evenodd"
+                                      d="M5.22 14.78a.75.75 0 001.06 0l7.22-7.22v5.69a.75.75 0 001.5 0v-7.5a.75.75 0 00-.75-.75h-7.5a.75.75 0 000 1.5h5.69l-7.22 7.22a.75.75 0 000 1.06z"
+                                      clip-rule="evenodd"
+                                    ></path>
+                                  </svg>
+                                </span>
+                              </span>
+                            </a>
+                          </div>
+                        </h3>
+                        <p className="mt-2 text-sm leading-normal">
+                          Skillfully integrated data privacy regulations,
+                          handled business admin tasks such as payroll and stock
+                          ordering, and assisted in policy development to
+                          enhance efficiency and customer satisfaction.
+                        </p>
+                      </div>
+                    </div>
+                  </li>
+                  <li className="mb-12">
+                    <div className="group relative grid pb-1 transition-all sm:grid-cols-8 sm:gap-8 md:gap-4 lg:hover:!opacity-100 lg:group-hover/list:opacity-50">
+                      <div className="absolute -inset-x-4 -inset-y-4 z-0 hidden rounded-md transition motion-reduce:transition-none lg:-inset-x-6 lg:block lg:group-hover:bg-slate-800/50 lg:group-hover:shadow-[inset_0_1px_0_0_rgba(148,163,184,0.1)] lg:group-hover:drop-shadow-lg"></div>
+                      <header
+                        className="z-10 mb-2 mt-1 text-xs font-semibold uppercase tracking-wide text-slate-500 sm:col-span-2"
+                        aria-label="July to December 2017"
+                      >
+                        Nov 2013 — Nov 2016
+                      </header>
+                      <div className="z-10 sm:col-span-6">
+                        <h3 className="font-medium leading-snug text-slate-200">
+                          <div>
+                            <a
+                              className="group/link inline-flex items-baseline text-base font-medium leading-tight text-slate-200  hover:text-teal-300 focus-visible:text-teal-300"
+                              href="https://www.townsville.health.qld.gov.au/"
+                              target="_blank"
+                              rel="noreferrer"
+                              aria-label="Storeman at Townsville Hospital"
+                            >
+                              <span className="absolute -inset-x-4 -inset-y-2.5 hidden rounded md:-inset-x-6 md:-inset-y-4 lg:block"></span>
+                              <span>
+                                Storeman ·{" "}
+                                <span className="inline-block">
+                                  Townsville Hospital & Health Service
+                                  <svg
+                                    xmlns="http://www.w3.org/2000/svg"
+                                    viewBox="0 0 20 20"
+                                    fill="currentColor"
+                                    className="ml-1 inline-block h-4 w-4 shrink-0 translate-y-px transition-transform group-hover/link:-translate-y-1 group-hover/link:translate-x-1 group-focus-visible/link:-translate-y-1 group-focus-visible/link:translate-x-1 motion-reduce:transition-none"
+                                    aria-hidden="true"
+                                  >
+                                    <path
+                                      fill-rule="evenodd"
+                                      d="M5.22 14.78a.75.75 0 001.06 0l7.22-7.22v5.69a.75.75 0 001.5 0v-7.5a.75.75 0 00-.75-.75h-7.5a.75.75 0 000 1.5h5.69l-7.22 7.22a.75.75 0 000 1.06z"
+                                      clip-rule="evenodd"
+                                    ></path>
+                                  </svg>
+                                </span>
+                              </span>
+                            </a>
+                          </div>
+                        </h3>
+                        <p className="mt-2 text-sm leading-normal">
+                          Efficiently managed the delivery, storage,
+                          preparation, and disposal of supplies and equipment,
+                          while successfully meeting urgent demands and ensuring
+                          strict compliance and top-notch quality.
+                        </p>
+                      </div>
+                    </div>
+                  </li>
+                  <li className="mb-12">
+                    <div className="group relative grid pb-1 transition-all sm:grid-cols-8 sm:gap-8 md:gap-4 lg:hover:!opacity-100 lg:group-hover/list:opacity-50">
+                      <div className="absolute -inset-x-4 -inset-y-4 z-0 hidden rounded-md transition motion-reduce:transition-none lg:-inset-x-6 lg:block lg:group-hover:bg-slate-800/50 lg:group-hover:shadow-[inset_0_1px_0_0_rgba(148,163,184,0.1)] lg:group-hover:drop-shadow-lg"></div>
+                      <header
+                        className="z-10 mb-2 mt-1 text-xs font-semibold uppercase tracking-wide text-slate-500 sm:col-span-2"
+                        aria-label="July to December 2017"
+                      >
+                        Nov 2011 — Jan 2013
+                      </header>
+                      <div className="z-10 sm:col-span-6">
+                        <h3 className="font-medium leading-snug text-slate-200">
+                          <div>
+                            <p
+                              className="group/link inline-flex items-baseline text-base font-medium leading-tight text-slate-200  hover:text-teal-300 focus-visible:text-teal-300"
+                              aria-label="Phone Technician at Mobile Phone Surgeon"
+                            >
+                              <span className="absolute -inset-x-4 -inset-y-2.5 hidden rounded md:-inset-x-6 md:-inset-y-4 lg:block"></span>
+                              <span>
+                                Phone Technician ·{" "}
+                                <span className="inline-block">
+                                  The Mobile Phone Surgeon
+                                </span>
+                              </span>
+                            </p>
+                          </div>
+                        </h3>
+                        <p className="mt-2 text-sm leading-normal">
+                          Effectively resolved mobile phone hardware and
+                          software issues, using OEM parts for replacements.
+                          Additionally, I maintained open communication with
+                          customers and prioritized data privacy throughout
+                          backup and restoration processes.
+                        </p>
+                      </div>
+                    </div>
+                  </li>
+                </ol>
                 <div className="mt-12">
                   <a
                     className="group inline-flex items-center font-semibold leading-tight text-slate-200"
@@ -206,7 +365,7 @@ export default function Home() {
                   >
                     <span>
                       <span className="border-b border-transparent pb-px transition group-hover:border-teal-300 motion-reduce:transition-none">
-                        View Full <wbr></wbr>
+                        View Full{" "}
                       </span>
                       <span className="whitespace-nowrap">
                         <span className="border-b border-transparent pb-px transition group-hover:border-teal-300 motion-reduce:transition-none">
@@ -229,6 +388,37 @@ export default function Home() {
                     </span>
                   </a>
                 </div>
+              </div>
+            </section>
+            <section
+              id="projects"
+              className="mb-16 scroll-mt-16 md:mb-24 lg:mb-36 lg:scroll-mt-24"
+              aria-label="projects"
+            >
+              <div className="sticky top-0 z-20 -mx-6 mb-4 w-screen bg-slate-900/75 px-6 py-5 backdrop-blur md:-mx-12 md:px-12 lg:sr-only lg:relative lg:top-auto lg:mx-auto lg:w-full lg:px-0 lg:py-0 lg:opacity-0">
+                <h2 className="text-sm font-bold uppercase tracking-widest text-slate-200 lg:sr-only">
+                  Projects
+                </h2>
+              </div>
+              <div>
+                <ul className="group/list">
+                  <li className="mb-12">
+                    <div className="group relative grid gap-4 pb-1 transition-all sm:grid-cols-8 sm:gap-8 md:gap-4 lg:hover:!opacity-100 lg:group-hover/list:opacity-50">
+                      <div className="absolute -inset-x-4 -inset-y-4 z-0 hidden rounded-md transition motion-reduce:transition-none lg:-inset-x-6 lg:block lg:group-hover:bg-slate-800/50 lg:group-hover:shadow-[inset_0_1px_0_0_rgba(148,163,184,0.1)] lg:group-hover:drop-shadow-lg"></div>
+                      <div className="z-10 sm:order-2 sm:col-span-6">
+                        <h3>
+                          <p
+                            className="group/link inline-flex items-baseline text-base font-medium leading-tight text-slate-200  hover:text-teal-300 focus-visible:text-teal-300"
+                            aria-label="Coming Soon"
+                          >
+                            <span className="absolute -inset-x-4 -inset-y-2.5 hidden rounded md:-inset-x-6 md:-inset-y-4 lg:block"></span>
+                            <span className="italic">Coming Soon </span>
+                          </p>
+                        </h3>
+                      </div>
+                    </div>
+                  </li>
+                </ul>
               </div>
             </section>
           </main>

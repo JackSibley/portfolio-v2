7690fb9a5b902df282a102e90b3dad484becda62
Jack Sibley <jacks@sibley.com.au>
2023-08-15 18:05:02 +1000

    updated footer text

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -423,7 +423,7 @@ export default function Home() {
             </section>
             <footer className="max-w-md pb-16 text-sm text-slate-500 sm:pb-0">
               <p>
-                Used this portfolio to learn{" "}
+                Used this portfolio to learn and implement{" "}
                 <a
                   href="https://tailwindcss.com/"
                   className="font-medium text-slate-400 hover:text-teal-300 focus-visible:text-teal-300"

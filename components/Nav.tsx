5adbb3b8517b82e364fc55e15ded7e247240e300
Jack Sibley <jacks@sibley.com.au>
2023-08-15 18:00:07 +1000

    updated navigation. Added footer

--- components/Nav.tsx
+++ components/Nav.tsx
@@ -1,7 +1,4 @@
-import { useScrollPosition } from "./../hooks/useScrollPosition";
-
 export const Nav = () => {
-  const scrollPosition = useScrollPosition();
   return (
     <nav className="nav hidden lg:block" aria-label="In-page jump links">
       <ul className="mt-16 w-max">
@@ -15,16 +12,16 @@ export const Nav = () => {
         </li>
         <li>
           <a className="group flex items-center py-3" href="#experience">
-            <span className="nav-indicator mr-4 h-px w-8 bg-slate-600 transition-all group-hover:w-16 group-hover:bg-slate-200 group-focus-visible:w-16 group-focus-visible:bg-slate-200 motion-reduce:transition-none"></span>
-            <span className="nav-text text-xs font-bold uppercase tracking-widest text-slate-500 group-hover:text-slate-200 group-focus-visible:text-slate-200">
+            <span className="mr-4 h-px w-8 bg-slate-600 transition-all group-hover:w-16 group-hover:bg-slate-200 group-focus-visible:w-16 group-focus-visible:bg-slate-200 motion-reduce:transition-none"></span>
+            <span className="text-xs font-bold uppercase tracking-widest text-slate-500 group-hover:text-slate-200 group-focus-visible:text-slate-200">
               Experience
             </span>
           </a>
         </li>
         <li>
           <a className="group flex items-center py-3" href="#projects">
-            <span className="nav-indicator mr-4 h-px w-8 bg-slate-600 transition-all group-hover:w-16 group-hover:bg-slate-200 group-focus-visible:w-16 group-focus-visible:bg-slate-200 motion-reduce:transition-none"></span>
-            <span className="nav-text text-xs font-bold uppercase tracking-widest text-slate-500 group-hover:text-slate-200 group-focus-visible:text-slate-200">
+            <span className="mr-4 h-px w-8 bg-slate-600 transition-all group-hover:w-16 group-hover:bg-slate-200 group-focus-visible:w-16 group-focus-visible:bg-slate-200 motion-reduce:transition-none"></span>
+            <span className="text-xs font-bold uppercase tracking-widest text-slate-500 group-hover:text-slate-200 group-focus-visible:text-slate-200">
               Projects
             </span>
           </a>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -421,6 +421,46 @@ export default function Home() {
                 </ul>
               </div>
             </section>
+            <footer className="max-w-md pb-16 text-sm text-slate-500 sm:pb-0">
+              <p>
+                Used this portfolio to learn{" "}
+                <a
+                  href="https://tailwindcss.com/"
+                  className="font-medium text-slate-400 hover:text-teal-300 focus-visible:text-teal-300"
+                  target="_blank"
+                  rel="noreferrer"
+                >
+                  Tailwind CSS.
+                </a>{" "}
+                Coded in{" "}
+                <a
+                  href="https://code.visualstudio.com/"
+                  className="font-medium text-slate-400 hover:text-teal-300 focus-visible:text-teal-300"
+                  target="_blank"
+                  rel="noreferrer"
+                >
+                  Visual Studio Code.
+                </a>{" "}
+                Built with{" "}
+                <a
+                  href="https://nextjs.org/"
+                  className="font-medium text-slate-400 hover:text-teal-300 focus-visible:text-teal-300"
+                  target="_blank"
+                  rel="noreferrer"
+                >
+                  Next.js{" "}
+                </a>
+                .Deployed with{" "}
+                <a
+                  href="https://vercel.com/"
+                  className="font-medium text-slate-400 hover:text-teal-300 focus-visible:text-teal-300"
+                  target="_blank"
+                  rel="noreferrer"
+                >
+                  Vercel
+                </a>
+              </p>
+            </footer>
           </main>
         </div>
       </div>

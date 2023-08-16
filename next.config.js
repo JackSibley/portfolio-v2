12c20ef121c37f33abc83319f7f28ddae18c308a
Jack Sibley <jacks@sibley.com.au>
2023-08-16 13:50:56 +1000

    removed rewrites

--- next.config.js
+++ next.config.js
@@ -1,13 +1,4 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {};
 
-module.exports = {
-  async rewrites() {
-    return [
-      {
-        source: "/hb",
-        destination: "198.13.51.27:8081",
-      },
-    ];
-  },
-};
+module.exports = nextConfig;
